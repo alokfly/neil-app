409adb3e756f623a01870b5c082f7e348ca6ca68
alok2110 <aloksaxena755@gmail.com>
2021-10-29 17:45:50 +0530

    new change

--- controllers/bidPurchaseController.js
+++ controllers/bidPurchaseController.js
@@ -7,19 +7,18 @@ const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN;
 const JWT_REFRESH_TOKEN = process.env.JWT_REFRESH_TOKEN;
 
 module.exports.bidPurchase = async (req, res) => {
-  const accessToken = req.cookies.accessToken;
-  const { bid, productId } = req.body;
-  if (accessToken) {
-    jwt.verify(accessToken, JWT_AUTH_TOKEN, async (err, email) => {
-      const { data } = email;
-      const { _id } = await User.findOne({ email: data });
-      const addBid = BidPurchase.create({
-        user_id: _id,
-        product_id: productId,
-        bid,
-      });
-      return res.status(200).send({ msg: "Bid successfully placed" });
+  const { bid, productId, email } = req.body;
+  try {
+    const { _id } = await User.findOne({ email });
+    console.log(_id);
+    const addBid = BidPurchase.create({
+      user_id: _id,
+      product_id: productId,
+      bid,
     });
+    return res.status(200).send({ msg: "Bid successfully placed" });
+  } catch (error) {
+    console.log(error);
   }
 };
 
--- routes/bidPurchaseRoutes.js
+++ routes/bidPurchaseRoutes.js
@@ -3,6 +3,6 @@ const auth = require("../utils/auth");
 const router = app.Router();
 const { authenticateUser } = require("../controllers/userController");
 const { bidPurchase, home } = require("../controllers/bidPurchaseController");
-router.post("/bidPurchase", authenticateUser, bidPurchase);
+router.post("/bidPurchase", bidPurchase);
 router.post("/home", authenticateUser, home);
 module.exports = router;
